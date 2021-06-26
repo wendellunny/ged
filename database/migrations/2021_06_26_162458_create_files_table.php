2b770dcdbcce18d4912104be12a3814fe6727e51
wendellunny <wendelferreiraportugal@gmail.com>
2021-06-26 13:34:49 -0300

    Migration e Model de Arquivos

---
+++ database/migrations/2021_06_26_162458_create_files_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('name');
+            $table->string('extension');
+            $table->int('size');
+            $table->int('structure_parent');
+            $table->string('user_upload');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
